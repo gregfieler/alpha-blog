5230307a3d6111578c369699a9c51756c695191e
Greg Fieler <gregfieler@gmail.com>
2021-01-19 23:27:22 -0800

    start going to fridge magnet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
     
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog, you have successfully signed up"
+      flash[:notice] = "Welcome to the Fridge, you have successfully signed up"
       redirect_to articles_path      
     else
       render 'new'
