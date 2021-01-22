d73fd6d1836f343972ad95e8beb15256979a65a9
Greg Fieler <gregfieler@gmail.com>
2021-01-21 16:53:49 -0800

    user dropdown NOT working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
     !! current_user
   end
 
+  def require_user
+    if !logged_in?
+      flash[:alert] = "you have to login to do that"
+      redirect_to login_path
+    end
+  end
+
 end
