f551f442527f07861633c2dcf0791fab38c1b362
Greg Fieler <gregfieler@gmail.com>
2021-01-23 18:19:25 -0800

    categories permission stuff lesson 193

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,39 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
-
+    @category = Category.new
   end
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5).order('created_at DESC')
   end
 
   def show
-
+    @category = Category.find(params[:id])
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "mmm K, we have a new category"
+      redirect_to @category
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "only admins can do that"
+      redirect_to categories_path
+    end
+  end
+
 end 
